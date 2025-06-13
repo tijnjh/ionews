4e160896295dbd7a0f2492653cda51e0f8c00a72
tijn <tijnjh@proton.me>
2025-06-13 10:50:07 +0200

    feat: prevent ios navigation gesture

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const element = document.querySelector(".prevent-swipe-nav");
+
+  element?.addEventListener("touchstart", (e) => {
+    // prevent swipe to navigate gesture
+    // @ts-expect-error
+    if (e.pageX > 20 && e.pageX < window.innerWidth - 20) return;
+    e.preventDefault();
+  });
+});
 </script>
 
 <template>
-  <ion-app>
+  <ion-app class="prevent-swipe-nav">
     <ion-router-outlet />
   </ion-app>
 </template>
